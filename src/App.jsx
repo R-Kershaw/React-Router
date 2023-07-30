2cb4b6130be2e241544225695f130d5afa6482d0
R-Kershaw <rkershaw90@gmail.com>
2023-07-30 10:10:09 -0400

    Completed Navbar tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import Blue from './components/Blue';
+import Red from './components/Red';
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <div id="main-section">
-      <Routes>
-        <Route path="/blue" element={<h1>Blue</h1>} />
-        <Route path="/red" element={<h1>Red</h1>} />
-      </Routes>
-    </div>
+    <>
+      <div id="navbar">
+        <Link to="/blue">blue</Link>
+        <Link to="/red">red</Link>
+        <Link to="/">home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   )
 }
